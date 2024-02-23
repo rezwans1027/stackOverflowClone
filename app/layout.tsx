706672c2fd3f4a014ca48b6f5b329646109516d5
rezwans1027 <rezwans1027@gmail.com>
2024-02-23 08:12:51 -0500

    add assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,51 @@
+import React from 'react'
 import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
-import React from 'react'
- 
+import { Inter, Space_Grotesk } from 'next/font/google'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'DevOverflow',
+  description: 'A community-driven platform for assking and answering programming questions.' +
+    'Get help, share knowledge, and collaborate with developers from around the world. Explore topics in' +
+    'web development, algorithms, data structures, and more.',
+  icons: {
+    icon: '/assets/images/site-logo.svg'
+  }
+}
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter'
+})
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-spaceGrotesk'
+})
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return (        
-    <ClerkProvider>
+  return (
+    <ClerkProvider
+    appearance={{
+      elements: {
+        formButtonPrimary: 'primary-gradient',
+        footerActionLink:
+        'primary-text-gradient hover:text-primary-500'
+      }
+    }}>
       <html lang="en">
-        <body>{children}</body>
+        <body
+          className={``}>
+          <h1 className='h1-bold'>Hello</h1>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   )
