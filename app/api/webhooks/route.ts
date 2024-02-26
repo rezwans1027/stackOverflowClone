3fe628a26d3551d8261163bd76932cde66fd13d8
rezwans1027 <rezwans1027@gmail.com>
2024-02-26 16:19:25 -0500

    updated user model

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -54,7 +54,6 @@ export async function POST(req: Request) {
 
   // Get the ID and type
   // eslint-disable-next-line no-unused-vars
-  const { id } = evt.data
   const eventType = evt.type;
 
   console.log("Event type: ", {eventType});
