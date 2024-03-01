ee3521787afe501b9e8d5884b7e28c204da8e5b0
rezwans1027 <rezwans1027@gmail.com>
2024-03-01 02:59:56 -0500

    fixed undefined localstorage error

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -1,48 +1,54 @@
 "use client"
 
-import React, { createContext, useContext, useState, useEffect } from "react"
+import React, { createContext, useContext, useState, useEffect } from "react";
 
 interface ThemeContextType {
-    mode: string
-    setMode: (mode: string) => void
+    mode: string;
+    setMode: (mode: string) => void;
 }
 
 const ThemeContext = createContext<ThemeContextType | undefined >(undefined);
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
-    const [mode, setMode] = useState(() => localStorage.getItem('theme') || 'light')
+    const [mode, setMode] = useState(() => {
+        if (typeof window !== 'undefined') {
+            return localStorage.getItem('theme') || 'light';
+        }
+        return 'light';
+    });
 
     const handleThemeChange = () => {
-        if (localStorage.theme === 'dark' || (!("theme" in localStorage)) && window.matchMedia("(prefers-color-scheme: dark)").matches) {
-            setMode('dark')
-            document.documentElement.classList.add('dark')
-            localStorage.setItem('theme', 'dark');
-        } else {
-            setMode('light')
-            document.documentElement.classList.remove('dark')
-            localStorage.setItem('theme', 'light');
+        if (typeof window !== 'undefined') {
+            if (localStorage.theme === 'dark' || (!("theme" in localStorage)) && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+                setMode('dark');
+                document.documentElement.classList.add('dark');
+                localStorage.setItem('theme', 'dark');
+            } else {
+                setMode('light');
+                document.documentElement.classList.remove('dark');
+                localStorage.setItem('theme', 'light');
+            }
         }
-    }
-
+    };
 
     useEffect(() => {
-        handleThemeChange()
+        handleThemeChange();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[mode])
+    },[mode]);
 
     return (
         <ThemeContext.Provider value={{ mode, setMode }}>
             {children}
         </ThemeContext.Provider>
-    )
+    );
 }
 
 export function useTheme() {
-    const context = useContext(ThemeContext)
+    const context = useContext(ThemeContext);
 
     if (context === undefined) {
-        throw new Error('useTheme must be used within a ThemeProvider')
+        throw new Error('useTheme must be used within a ThemeProvider');
     }
 
-    return context
+    return context;
 }
